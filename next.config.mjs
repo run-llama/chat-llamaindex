b394d18c9a7f1bef2a4f5d693205ef57df887b68
Marcus Schiesser <mail@marcusschiesser.de>
2023-10-17 12:00:13 +0700

    fix: add cache folder to vercel

--- next.config.mjs
+++ next.config.mjs
@@ -10,6 +10,9 @@ const nextConfig = {
   },
   experimental: {
     serverComponentsExternalPackages: ["pdf-parse", "llamaindex"],
+    outputFileTracingIncludes: {
+      "/*": ["./cache/**/*"],
+    },
   },
 };
 
