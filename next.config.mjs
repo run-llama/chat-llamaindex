86aca07c475414abe2f3c46964294e654d7fc78b
Marcus Schiesser <mail@marcusschiesser.de>
2023-11-27 15:28:38 +0700

    feat: added sentry

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,4 @@
+import {withSentryConfig} from "@sentry/nextjs";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
@@ -16,4 +17,30 @@ const nextConfig = {
   },
 };
 
-export default nextConfig;
+export default withSentryConfig(nextConfig, {
+// For all available options, see:
+// https://github.com/getsentry/sentry-webpack-plugin#options
+
+// Suppresses source map uploading logs during build
+silent: true,
+org: "llamaindex",
+project: "chat-llamaindex",
+}, {
+// For all available options, see:
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+
+// Upload a larger set of source maps for prettier stack traces (increases build time)
+widenClientFileUpload: true,
+
+// Transpiles SDK to be compatible with IE11 (increases bundle size)
+transpileClientSDK: true,
+
+// Routes browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers (increases server load)
+tunnelRoute: "/monitoring",
+
+// Hides source maps from generated client bundles
+hideSourceMaps: true,
+
+// Automatically tree-shake Sentry logger statements to reduce bundle size
+disableLogger: true,
+});
