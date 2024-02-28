85e017ada4b0754faba28c7eaf7ac2acffb20be0
Marcus Schiesser <mail@marcusschiesser.de>
2024-02-28 17:04:47 +0700

    add serverComponentsExternalPackages for pdf-parse

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,7 @@ import { withSentryConfig } from "@sentry/nextjs";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
+    serverComponentsExternalPackages: ["pdf-parse"],
     outputFileTracingIncludes: {
       "/*": ["./cache/**/*"],
     },
