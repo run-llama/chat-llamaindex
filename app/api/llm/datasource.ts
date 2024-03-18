4423823bf5508ec05f32e36f84a8f4260e9f2fd9
Marcus Schiesser <mail@marcusschiesser.de>
2024-03-18 16:28:08 +0700

    feat: use edge runtime

--- app/api/llm/datasource.ts
+++ app/api/llm/datasource.ts
@@ -1,10 +1,7 @@
 import { DATASOURCES_CACHE_DIR } from "@/scripts/constants.mjs";
-import {
-  VectorStoreIndex,
-  storageContextFromDefaults,
-  ServiceContext,
-  SimpleDocumentStore,
-} from "llamaindex";
+import { VectorStoreIndex, ServiceContext } from "@llamaindex/edge";
+import { storageContextFromDefaults } from "@llamaindex/edge/storage/StorageContext";
+import { SimpleDocumentStore } from "@llamaindex/edge/storage/docStore/SimpleDocumentStore";
 
 export async function getDataSource(
   serviceContext: ServiceContext,
