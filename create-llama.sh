71a789755d6f1cf8fff10653d37fc981a14b193f
Thuc Pham <51660321+thucpn@users.noreply.github.com>
2024-06-13 18:10:40 +0700

    feat: use api from create-llama

--- create-llama.sh
+++ create-llama.sh
@@ -5,9 +5,12 @@ echo -e "\nCreating Llama App..."
 # Go to the app directory
 cd app
 
-# Remove any existing chat-session directory
-rm -rf create_llama
+# Remove any existing copy
 rm -rf components/chat/chat-session
+rm -rf api/chat
+rm -rf api/files
+rm -rf observability
+rm -rf ../config
 
 # Run the node command with specified options
 npx -y create-llama@0.1.10 \
@@ -26,9 +29,14 @@ npx -y create-llama@0.1.10 \
     --use-pnpm \
     -- create_llama
 
-# copy components
+# copy components and apis
 mkdir -p components/chat/chat-session
+mkdir -p observability
+mkdir -p ../config
 cp -r create_llama/app/components/* components/chat/chat-session
+cp -r create_llama/app/api/* api
+cp -r create_llama/app/observability/* observability
+cp -r create_llama/config/* ../config
 
 # Clean up unnecessary files
 rm -rf create_llama
