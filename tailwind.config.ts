7a02de4ccd1cf09461b167affad45c0ff4e1872e
Thuc Pham <51660321+thucpn@users.noreply.github.com>
2024-06-20 13:24:49 +0700

    fix: tailwind should parse the whole component folder

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,7 +6,7 @@ module.exports = {
     './components/**/*.{ts,tsx}',
     './app/**/*.{ts,tsx}',
     './src/**/*.{ts,tsx}',
-    './cl/app/components/ui/chat/**/*.{ts,tsx}'
+    './cl/app/components/**/*.{ts,tsx}'
 	],
   theme: {
     container: {
