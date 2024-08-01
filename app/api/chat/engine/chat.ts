79c0bebcf3ef8e2572d1de03fd0390b3f9c7a64a
Thuc Pham <51660321+thucpn@users.noreply.github.com>
2024-08-01 18:02:40 +0700

    refactor: import generate filters from create-llama

--- app/api/chat/engine/chat.ts
+++ app/api/chat/engine/chat.ts
@@ -1,10 +1,6 @@
-import {
-  ContextChatEngine,
-  MetadataFilter,
-  MetadataFilters,
-  Settings,
-} from "llamaindex";
+import { ContextChatEngine, Settings } from "llamaindex";
 import { getDataSource } from "./index";
+import { generateFilters } from "@/cl/app/api/chat/engine/chat";
 
 interface ChatEngineOptions {
   datasource?: string;
@@ -31,28 +27,4 @@ export async function createChatEngine({
     retriever,
     systemPrompt: process.env.SYSTEM_PROMPT,
   });
-}
-
-function generateFilters(documentIds: string[]): MetadataFilters {
-  // public documents don't have the "private" field or it's set to "false"
-  const publicDocumentsFilter: MetadataFilter = {
-    key: "private",
-    value: ["true"],
-    operator: "nin",
-  };
-
-  // if no documentIds are provided, only retrieve information from public documents
-  if (!documentIds.length) return { filters: [publicDocumentsFilter] };
-
-  const privateDocumentsFilter: MetadataFilter = {
-    key: "doc_id",
-    value: documentIds,
-    operator: "in",
-  };
-
-  // if documentIds are provided, retrieve information from public and private documents
-  return {
-    filters: [publicDocumentsFilter, privateDocumentsFilter],
-    condition: "or",
-  };
 }
